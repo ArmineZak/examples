75d8d1c3b36d3ab9aac46e63c3a7e43add7f68ad
ArmineZak <armzak91@gmail.com>
2020-07-20 10:39:44 +0400

    -added new module for arithmetic operators' examples

---
+++ Arithmetic-Operators/src/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        double num1 = 12.5, num2 = 3.5, result;
+
+        result = num1 + num2;
+        System.out.println("num1 + num2 = " + result);
+
+        result = num1 - num2;
+        System.out.println("num1 - num2 = " + result);
+
+        result = num1 * num2;
+        System.out.println("num1 * num2 = " + result);
+
+        result = num1 / num2;
+        System.out.println("num1 / num2 = " + result);
+
+        result = num1 % num2;
+        System.out.println("num1 % num2 = " + result);
+
+    }
+}
