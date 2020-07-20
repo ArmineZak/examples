8e0675ff297bee7a109097b8b2481fd12f422ecc
ArmineZak <armzak91@gmail.com>
2020-07-20 11:21:34 +0400

    -added new module for equity and relational operator examples

---
+++ Equity-And-Relational-Operators/src/RelationalOperator.java
@@ -0,0 +1,13 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+
+        int num1 = 5, num2 = 6;
+
+        if(num1 > num2) {
+            System.out.println("num1 is grater than num2.");
+        }
+        else {
+            System.out.println("num2 is greater than num1.");
+        }
+    }
+}
